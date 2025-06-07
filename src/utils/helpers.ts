2ae9ae38cf6b20a0a19403c9b8211a01ef03e0fc
Rendee Lloren <86382091+jueviolegrace13@users.noreply.github.com>
2025-06-07 14:42:19 +0800

    Updated helpers.ts

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -77,6 +77,6 @@ export const createDateString = (): string => {
 
 
 // Create a new date string in ISO format
-export const generateId() = (): string => {
+export const generateId = (): string => {
   return 'id-' + Math.random().toString(36).substr(2, 9) + '-' + Date.now();
 }
